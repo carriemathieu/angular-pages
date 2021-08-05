34a6ea4b43592cded96c12f9be368cab95048b82
Carrie Mathieu <carrieburks@gmail.com>
2021-08-05 14:16:57 -0400

    create times directive

---
+++ src/app/times.directive.ts
@@ -0,0 +1,26 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appTimes]'
+})
+export class TimesDirective {
+
+  // viewcontainer - reference to container directive applied to
+  // templateref - reference to whatever elements directive applied to 
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) { }
+  
+  // "whenever 'apptimes' directive set on an element..."
+  @Input('appTimes') set render(times: number){
+    this.viewContainer.clear();
+
+    // from 0 to times, create new instance of templateref
+    for (let i = 0; i < times; i++) {
+      // createEmbeddedview = create element, referencing templateRef
+      this.viewContainer.createEmbeddedView(this.templateRef, {})
+    }
+  }
+}
