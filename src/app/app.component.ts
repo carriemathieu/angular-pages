a5e112d61b28ebbe6608bc170a271489e44479f2
Carrie Mathieu <carrieburks@gmail.com>
2021-08-05 11:50:47 -0400

    add pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage = 0;
+
   images = [
     {
       title: "Falling Leaves",
