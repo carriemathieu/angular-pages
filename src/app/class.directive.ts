9df5239b66f39aa87c5bb6c9da0678b232d3d886
Carrie Mathieu <carrieburks@gmail.com>
2021-08-05 14:00:24 -0400

    add loging to class directive

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
+// inside html template, add "appClass" to whatever element we want to apply directive to 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
-
-  constructor() { }
-
+  // element - direct reference to LI element in HTML template
+  constructor(private element:ElementRef) {}
+  
+  // @input allows property to be set by parent componenet
+  // allows us to "intercept" whenever angular tries to set background color
+  @Input() set backgroundColor(color: string) {
+    // nativeElement - actual element (li)
+    this.element.nativeElement.style.backgroundColor = color;
+  }
 }
